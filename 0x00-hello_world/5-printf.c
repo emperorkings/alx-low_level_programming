629d1de66dac2365c75b3d44a6571d4816a62182
emperorkings <mperorkings@gmail.com>
2023-08-11 02:27:54 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
-*
-* Return: Always 0 (Success)
-*/
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+    printf("When words fail, art speaks.\n");
     return (0);
 }
