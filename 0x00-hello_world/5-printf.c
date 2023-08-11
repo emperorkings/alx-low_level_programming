78183aa075b637fee6fd8a1ea8e055b983a9f4b7
emperorkings <mperorkings@gmail.com>
2023-08-11 02:03:28 +0100

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point 
+ * main - A programme that printa with printf 
  * Return: 0 (passed)
- */
+ *
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,"\n)
-		return (0);
+	return (0);
 }
