02c63c17aad1b517382ccf04df694119172241d2
emperorkings <mperorkings@gmail.com>
2023-08-11 01:02:43 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C prpgram that prints a linenwith value 0
+ * Return: 0(positive)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
