50da304162d691a20af1c314cce88132a0126ff1
emperorkings <mperorkings@gmail.com>
2023-08-11 02:22:33 +0100

    programme with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
  *
@@ -10,4 +9,3 @@ int main(void)
     printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
-
