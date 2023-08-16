68d679c309136276200719595a621425a45beb27
emperorkings <mperorkings@gmail.com>
2023-08-16 11:37:20 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description : a prpgram that prints a putchar in a new line.
+ * return: always 0 (successful).
+ *
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
+
