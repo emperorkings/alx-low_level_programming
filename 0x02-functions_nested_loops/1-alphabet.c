c74d9a20afe5e9a5282c9dd85aa92e2dfb84017a
emperorkings <mperorkings@gmail.com>
2023-08-16 17:53:38 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ * Resume:void
+ *
+*/
+void print_alphabet(0);
+{
+	char letter;
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter)
+		}
+	_putchar('\n');
+}
