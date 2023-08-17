41c459812abeba8190e57b4757fd51d85cd59a8b
emperorkings <mperorkings@gmail.com>
2023-08-16 18:08:47 -0700

    1 aphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  * Resume:void
  *
 */
-void print_alphabet(0);
+void print_alphabet(void)
 {
 	char letter;
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
-			_putchar(letter)
+			_putchar(letter);
 		}
 	_putchar('\n');
 }
