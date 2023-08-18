9effcdaed10741ba2b3ccb6c9e3431818f516223
emperorkings <mperorkings@gmail.com>
2023-08-18 14:27:44 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: The character to be checked.
  * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise.
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
