5436fffb86850acd36d8fe9059a0a9ac314802cb
emperorkings <mperorkings@gmail.com>
2023-08-18 16:12:29 -0700

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _abs - Determine the actual value
+  * @d: integer input
+  * Return: the absolute value
+*/
+
+int _abs(int d)
+{
+	if (d < 0)
+	{
+		return (-d);
+	}
+	else
+	{
+		return (d);
+	}
+}
+
