5c153663c5bb072afeedf4266c974b4530f8c58c
emperorkings <mperorkings@gmail.com>
2023-08-21 10:48:09 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,18 +7,16 @@ void print_rev(char *s)
 {
 	int length = 0;
 
-	/* Calculate the length of the string */
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	/* Print the string in reverse */
 	while (length > 0)
 	{
 		length--;
 		putchar(s[length]);
 	}
 
-	putchar('\n'); /* Print a newline character at the end */
+	putchar('\n');
 }
