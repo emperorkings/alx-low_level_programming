2ba8c60597c6e13e670b2ecc57c0c0a35e128713
emperorkings <mperorkings@gmail.com>
2023-08-21 10:26:37 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,9 +7,9 @@
 void _puts(char *str)
 {
 	while (*str != '\0')
-	{
-		_putchar(str++);
-	}
-	_putchar('\n');
+		putchar(str++);
+}
+
+	putchar('\n');
 }
 
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line.
+ * @s: The string to be printed in reverse.
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	/* Print the string in reverse */
+	for (int i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+
+	putchar('\n'); /* Print a newline character at the end */
+}
+
