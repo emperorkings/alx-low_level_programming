4e47398aaae46830346afde2bb8692646ecca3f9
emperorkings <mperorkings@gmail.com>
2023-08-21 15:04:01 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,13 +2,13 @@
 
 /**
  * puts2 - Prints every other character of a string.
- * @str: The string to be printed.
+ * @str: The input string to print from.
  */
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (str != NULL && str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		putchar(str[i]);
 		i += 2;
