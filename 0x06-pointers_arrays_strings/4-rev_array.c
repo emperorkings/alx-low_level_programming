c035a21847b487782f43f231eec8381dc44bad1e
emperorkings <mperorkings@gmail.com>
2023-08-23 14:35:21 -0700

    -Wall -pedanti

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,17 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j;
+	int i = 0;
+	int j = n - 1;
 
-	for (i = 0, j = n - 1; i < j; i++, j--)	
+	while (i < j)
+	{
 		a[i] ^= a[j];
 		a[j] ^= a[i];
 		a[i] ^= a[j];
+
+		i++;
+		j--;
 	}
 }
 
