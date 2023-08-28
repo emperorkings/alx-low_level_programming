5045da4183d20b7c3f0f03c743c6e76d112ecf01
emperorkings <mperorkings@gmail.com>
2023-08-28 12:26:21 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strchr - Locates the first occurrence of a character in a string.
  * @s: The string to search in.
@@ -21,4 +22,3 @@ char *_strchr(char *s, char c)
 
 	return (0);
 }
-
